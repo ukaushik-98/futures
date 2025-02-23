19d7328e14135be81a7899219c6bb72bae262d68
ukaushik-98 <kaushikuk101@gmail.com>
2025-02-22 17:42:40 -0800

    restructure

---
+++ src/futures_lifetimes/mod.rs
@@ -0,0 +1,33 @@
+use std::pin::Pin;
+
+pub trait Service<Request> {
+    type Response;
+    type Error;
+    type Future<'a, 'b>: Future<Output = Result<Self::Response, Self::Error>>
+    where
+        Self: 'a + 'b;
+
+    fn call<'a, 'b>(&'a mut self, req: Request) -> Self::Future<'a, 'b>;
+}
+
+pub struct MyRequest;
+pub struct MyResponse;
+pub struct MyError;
+#[derive(Debug)]
+pub struct MyService;
+
+impl Service<MyRequest> for MyService {
+    type Response = MyResponse;
+    type Error = MyError;
+    type Future<'a, 'b> =
+        Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>> + Send + 'b>>;
+
+    fn call<'a, 'b>(&'a mut self, req: MyRequest) -> Self::Future<'a, 'b> {
+        Box::pin(async move {
+            // println! ("{:?}", self);
+            Ok(MyResponse)
+        })
+    }
+}
+
+fn static_check<T: 'static>(t: T) {}
--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,4 @@
-use std::pin::Pin;
-
-trait Service<Request> {
-    type Response;
-    type Error;
-    type Future<'a, 'b>: Future<Output = Result<Self::Response, Self::Error>>
-    where
-        Self: 'a + 'b;
-
-    fn call<'a, 'b>(&'a mut self, req: Request) -> Self::Future<'a, 'b>;
-}
-
-struct MyRequest;
-struct MyResponse;
-struct MyError;
-#[derive(Debug)]
-struct MyService;
-
-impl Service<MyRequest> for MyService {
-    type Response = MyResponse;
-    type Error = MyError;
-    type Future<'a, 'b> =
-        Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>> + Send + 'b>>;
-
-    fn call<'a, 'b>(&'a mut self, req: MyRequest) -> Self::Future<'a, 'b> {
-        Box::pin(async move {
-            // println! ("{:?}", self);
-            Ok(MyResponse)
-        })
-    }
-}
-
-fn static_check<T: 'static>(t: T) {}
+use futures::futures_lifetimes::{MyRequest, MyService, Service};
 
 #[tokio::main]
 async fn main() {
