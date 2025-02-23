e1e7abf18b2b6657faf77b3fdc5d9c858a755873
ukaushik-98 <kaushikuk101@gmail.com>
2025-02-22 17:38:58 -0800

    futures with gat

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+use std::pin::Pin;
+
+trait Service<Request> {
+    type Response;
+    type Error;
+    type Future<'a, 'b>: Future<Output = Result<Self::Response, Self::Error>>
+    where
+        Self: 'a + 'b;
+
+    fn call<'a, 'b>(&'a mut self, req: Request) -> Self::Future<'a, 'b>;
+}
+
+struct MyRequest;
+struct MyResponse;
+struct MyError;
+#[derive(Debug)]
+struct MyService;
+
+impl Service<MyRequest> for MyService {
+    type Response = MyResponse;
+    type Error = MyError;
+    type Future<'a, 'b> =
+        Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>> + Send + 'b>>;
+
+    fn call<'a, 'b>(&'a mut self, req: MyRequest) -> Self::Future<'a, 'b> {
+        Box::pin(async move {
+            // println! ("{:?}", self);
+            Ok(MyResponse)
+        })
+    }
+}
+
+fn static_check<T: 'static>(t: T) {}
+
+#[tokio::main]
+async fn main() {
+    let mut m = MyService;
+    let mr = m.call(MyRequest);
+    let _ = tokio::spawn(mr).await;
+}
