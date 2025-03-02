a1a418dc6f0a25aaa23ab83b1c9a906d5eab1407
ukaushik-98 <kaushikuk101@gmail.com>
2025-03-02 13:59:33 -0800

    use move

---
+++ src/futures_lifetimes/foo.rs
@@ -0,0 +1,46 @@
+use std::{fmt::Debug, ops::DerefMut, pin::Pin};
+
+pub trait Service<Request> {
+    type Response;
+    type Error;
+    type Future<'a>: Future<Output = Result<Self::Response, Self::Error>>
+    where
+        Self: 'a;
+
+    fn call<'a>(&'a mut self, req: Request) -> Self::Future<'a>;
+}
+
+struct FooService<'a, T> {
+    url: &'a T,
+}
+
+impl<'a, T, Request> Service<Request> for FooService<'a, T>
+where
+    T: Debug + Sync,
+{
+    type Response = &'a T;
+
+    type Error = ();
+
+    type Future<'b>
+        = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>> + Send + 'b>>
+    where
+        Self: 'b;
+
+    fn call(&mut self, req: Request) -> Self::Future<'_> {
+        Box::pin(async move {
+            println!("{:?}", self.url);
+            Ok(self.url)
+        })
+    }
+}
+
+async fn runner(v: &'static Vec<i32>) {
+    // let v = vec![1, 2, 3];
+    let mut foo = FooService { url: v };
+    // putting stuff in foo and moving it will cause issues
+    let s = tokio::spawn(async move {
+        let x = foo.call(());
+    });
+    println!("{:?}", v);
+}
